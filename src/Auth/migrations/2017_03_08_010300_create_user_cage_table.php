c30a56498be14905d14a271e6c2aaa6b58c9779d
Sam Newsome <samuel.a.newsome@gmail.com>
2017-08-21 22:30:30 +0100

    Add IP column to DB

--- src/Auth/migrations/2017_03_08_010300_create_user_cage_table.php
+++ src/Auth/migrations/2017_03_08_010300_create_user_cage_table.php
@@ -22,6 +22,7 @@ class CreateUserCageTable extends Migration
 
             $table->unsignedInteger('issuer_id')->nullable();
             $table->unsignedInteger('user_id')->nullable();
+            $table->unsignedInteger('user_ipv4')->nullable();
 
             /** @see \Laranix\Auth\User\Cage\Settings */
             $table->unsignedTinyInteger('cage_status')->default(1);
