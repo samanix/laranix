233232c1d16633f42b99e6761a976e93cbd80b78
Sam Newsome <samanixcom@gmail.com>
2017-09-03 14:06:53 +0100

    Update subsplit

--- publish/subsplit.sh
+++ publish/subsplit.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 cd ..
-git subsplit init git@github.com:samanix/laranix.git
-git subsplit publish src/Laranix/AntiSpam:git@github.com:laranix/AntiSpam.git
-git subsplit publish src/Laranix/Auth:git@github.com:laranix/auth.git
-git subsplit publish src/Laranix/Foundation:git@github.com:laranix/Foundation.git
-git subsplit publish src/Laranix/Installer:git@github.com:laranix/Installer.git
-git subsplit publish src/Laranix/Session:git@github.com:laranix/Session.git
-git subsplit publish src/Laranix/Support:git@github.com:laranix/Support.git
-git subsplit publish src/Laranix/Themer:git@github.com:laranix/Themer.git
-git subsplit publish src/Laranix/Tracker:git@github.com:laranix/Tracker.git
-rm -rf .subsplit/
+git subsplit update
+
+git subsplit publish "
+    src/Laranix/AntiSpam:git@github.com:laranix/AntiSpam.git
+    src/Laranix/Auth:git@github.com:laranix/auth.git
+    src/Laranix/Foundation:git@github.com:laranix/Foundation.git
+    src/Laranix/Installer:git@github.com:laranix/Installer.git
+    src/Laranix/Session:git@github.com:laranix/Session.git
+    src/Laranix/Support:git@github.com:laranix/Support.git
+    src/Laranix/Themer:git@github.com:laranix/Themer.git
+    src/Laranix/Tracker:git@github.com:laranix/Tracker.git
+" --heads="master 2.0"
