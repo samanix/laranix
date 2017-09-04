59ca8e6d6a8e4788759880a9d5b0a9036ee76070
Sam Newsome <samanixcom@gmail.com>
2017-09-04 21:45:58 +0100

    Rolled own Authorizable Remove laravel/framework dependancy

--- src/Laranix/Auth/User/User.php
+++ src/Laranix/Auth/User/User.php
@@ -17,7 +17,6 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Foundation\Auth\Access\Authorizable;
 
 class User extends Model implements
     AuthenticatableContract,
