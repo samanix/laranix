86137a0366571d493fad8512f7f0053a900cffda
Sam Newsome <sam@samanix.com>
2017-12-04 21:11:29 +0000

    Fix namespace

--- src/Laranix/Themer/ServiceProvider.php
+++ src/Laranix/Themer/ServiceProvider.php
@@ -4,7 +4,7 @@ namespace Laranix\Themer;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Laranix\Support\IO\Url\Url;
 use Laranix\Themer\Commands\ClearCompiled;
-use Laranix\Themer\Commands\LinkImages;
+use Laranix\Themer\Commands\LinkDirectory;
 use Laranix\Themer\Images\Images;
 use Laranix\Themer\Scripts\Scripts;
 use Laranix\Themer\Styles\Styles;
