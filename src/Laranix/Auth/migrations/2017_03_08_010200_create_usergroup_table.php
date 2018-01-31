4024a7a6038a11c487b59274cbcf1b1d9e4d9e77
Sam Newsome <sam@samanix.com>
2018-01-31 17:17:26 +0000

    Fix class names

--- src/Laranix/Auth/migrations/2017_03_08_010200_create_usergroup_table.php
+++ src/Laranix/Auth/migrations/2017_03_08_010200_create_usergroup_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserGroupsTable extends Migration
+class CreateUserGroupTable extends Migration
 {
     /**
      * Run the migrations
