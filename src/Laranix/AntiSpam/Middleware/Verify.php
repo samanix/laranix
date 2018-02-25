a6e382b5ec2885ffccd7388f1937e0900dbaed1e
Sam Newsome <sam@samanix.com>
2018-02-25 21:26:46 +0000

    Add check/override for ignoring recaptcha

--- src/Laranix/AntiSpam/Middleware/Verify.php
+++ src/Laranix/AntiSpam/Middleware/Verify.php
@@ -9,14 +9,32 @@ use Laranix\Support\Request\Middleware;
 class Verify extends Middleware
 {
     /**
+     * Handle request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     * @param bool                     $recaptcha
+     * @return mixed
+     */
+    public function handle($request, Closure $next, bool $recaptcha = true)
+    {
+        if ($this->shouldExecute($request)) {
+            return $this->before($request, $next, $recaptcha);
+        }
+
+        return $next($request);
+    }
+
+    /**
      * Process middleware.
      * Write this as you would a normal handle method for laravel middleware
      *
      * @param \Illuminate\Http\Request $request
      * @param \Closure                 $next
+     * @param bool                     $recaptcha
      * @return mixed
      */
-    protected function before($request, Closure $next)
+    protected function before($request, Closure $next, bool $recaptcha = true)
     {
         $sequence   = $this->app->make(Sequence::class);
 
@@ -24,10 +42,12 @@ class Verify extends Middleware
             return $sequence->redirect();
         }
 
-        $recaptcha  = $this->app->make(Recaptcha::class);
+        if ($recaptcha) {
+            $recaptcha = $this->app->make(Recaptcha::class);
 
-        if (!$recaptcha->verify()) {
-            return $recaptcha->redirect();
+            if (!$recaptcha->verify()) {
+                return $recaptcha->redirect();
+            }
         }
 
         return $next($request);
